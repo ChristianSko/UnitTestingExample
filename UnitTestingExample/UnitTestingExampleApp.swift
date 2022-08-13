2753c912441fdf0d055a74e5061a39e859a616c3
Christian Skorobogatow <christiansko@icloud.com>
2022-08-13 16:00:23 +0200

    Initial Commit

---
+++ UnitTestingExample/UnitTestingExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  UnitTestingExampleApp.swift
+//  UnitTestingExample
+//
+//  Created by Christian Skorobogatow on 13/8/22.
+//
+
+import SwiftUI
+
+@main
+struct UnitTestingExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
